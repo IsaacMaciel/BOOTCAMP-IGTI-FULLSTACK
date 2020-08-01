aa23fc2b6302fe6701eb6481c9552db438e81a39
Isaac Araujo <isaac_maciel@hotmail.com>
2020-08-01 13:44:00 -0300

    FInalizacao do exercicio, pedindo futuras correções

--- modulo2/trabPratico/index.js
+++ modulo2/trabPratico/index.js
@@ -3,81 +3,148 @@ const { promises } = require("fs");
 const fs = promises;
 
 const readJsonCityState = async () => {
-    const arrayCity = JSON.parse(await fs.readFile("Cidades.json"));
-    const arrayState = JSON.parse(await fs.readFile("Estados.json"));
+  const arrayCity = JSON.parse(await fs.readFile("Cidades.json"));
+  const arrayState = JSON.parse(await fs.readFile("Estados.json"));
 
-    return {
-        arrayCity,
-        arrayState,
-    };
+  return {
+    arrayCity,
+    arrayState,
+  };
 };
 
 const runCities = async () => {
-    const { arrayCity, arrayState } = await readJsonCityState();
-    arrayState.forEach(async ({ Nome }) => {
-        let cityFiltered = arrayCity.filter(
-            (city) => parseInt(city.Estado) === parseInt(state.ID)
-        );
-        await fs.writeFile(
-            `./filtered/${Nome}.json`,
-            JSON.stringify(cityFiltered)
-        );
-    });
+  const { arrayCity, arrayState } = await readJsonCityState();
+  arrayState.forEach(async ({ Nome }) => {
+    let cityFiltered = arrayCity.filter(
+      (city) => parseInt(city.Estado) === parseInt(state.ID)
+    );
+    await fs.writeFile(`./filtered/${Nome}.json`, JSON.stringify(cityFiltered));
+  });
 };
 
 // runCities();
 
-const readCity = async (UF) => {
-    const { arrayState } = await readJsonCityState();
-    const { Sigla, Nome } = arrayState.find((element) => element.Sigla === UF);
-    const data = JSON.parse(await fs.readFile(`./filtered/${Nome}.json`));
-    return data.length;
+const readCityQuantity = async (UF) => {
+  const { arrayState } = await readJsonCityState();
+  const { Sigla, Nome } = arrayState.find((element) => element.Sigla === UF);
+  const data = JSON.parse(await fs.readFile(`./filtered/${Nome}.json`));
+  return data.length;
+};
+
+const readCityForUF = async (UF) => {
+  const { arrayState } = await readJsonCityState();
+  const { Nome } = arrayState.find((element) => element.Sigla === UF);
+  const data = JSON.parse(await fs.readFile(`./filtered/${Nome}.json`));
+  return data;
 };
 
 const topFive = async (option) => {
-    let arrayStateCityQuantity = [];
+  let arrayStateCityQuantity = [];
 
-    const { arrayState } = await readJsonCityState();
+  const { arrayState } = await readJsonCityState();
 
-    for (const { Sigla } of arrayState) {
-        const quantity = await readCity(Sigla);
-        arrayStateCityQuantity = [
-            ...arrayStateCityQuantity,
-            { UF: Sigla, QtdCidades: quantity },
-        ];
-    }
+  for (const { Sigla } of arrayState) {
+    const quantity = await readCityQuantity(Sigla);
+    arrayStateCityQuantity = [
+      ...arrayStateCityQuantity,
+      { UF: Sigla, QtdCidades: quantity },
+    ];
+  }
 
-    arrayStateCityQuantity = arrayStateCityQuantity
-        .sort((a, b) => {
-            if (option == "More") {
-                return b.QtdCidades - a.QtdCidades;
-            } else {
-                return a.QtdCidades - b.QtdCidades;
-            }
-        })
-        .slice(0, 5);
+  arrayStateCityQuantity = arrayStateCityQuantity
+    .sort((a, b) => {
+      if (option == "More") {
+        return b.QtdCidades - a.QtdCidades;
+      } else {
+        return a.QtdCidades - b.QtdCidades;
+      }
+    })
+    .slice(0, 5);
+  option
+    ? console.log(arrayStateCityQuantity)
+    : console.log(arrayStateCityQuantity.reverse());
+};
+const stringCity = async (option) => {
+  const { arrayCity, arrayState } = await readJsonCityState();
 
-    console.log(arrayStateCityQuantity);
+  let cityString = null;
+  let state = null;
+
+  const orderedCity = arrayCity
+    .map((city) => {
+      return {
+        ...city,
+        nameLenght: city.Nome.length,
+      };
+    })
+    .sort();
+
+  cityString = orderedCity
+    .sort((a, b) => {
+      if (option === "More") {
+        return b.nameLenght - a.nameLenght;
+      }
+      return a.nameLenght - b.nameLenght;
+    })
+    .slice(0,100);
+    cityString.sort((a,b) => {
+        return a.Nome.localeCompare(b.Nome)
+    })
+
+  state = arrayState.find(({ ID }) => ID === cityString[0].Estado);
+
+console.log(`${cityString[0].Nome} -  ${state.Sigla}`);
+
+
 };
 
-const moreStringCity = async () => {
-    let cities = [];
-    const { arrayState } = await readJsonCityState();
-    for (const { Nome } of arrayState) {
-        let moreString = [];
-        cities = JSON.parse(await fs.readFile(`./filtered/${Nome}.json`));
-        moreString = cities
-            .sort((a, b) => {
-                return a.Nome.localeCompare(b.Nome);
-            })
-            .slice(0, 1);
+const largeCityForUf = async (option) => {
+  const { arrayState } = await readJsonCityState();
+  let largeCities = [];
+  let largeCitiesFormatted = [];
+  let stringArray = "";
 
-        cities = [...cities, moreString];
+  for (const { Sigla } of arrayState) {
+    let stringCity = [];
+    const cityArray = await readCityForUF(Sigla);
+    const orderedCity = cityArray
+      .map((city) => {
+        return {
+          ...city,
+          nameLenght: city.Nome.length,
+        };
+      })
+      .sort();
+
+    stringCity = orderedCity
+      .sort((a, b) => {
+        if (option === "More") {
+          return b.nameLenght - a.nameLenght;
+        }
+        return a.nameLenght - b.nameLenght;
+      })
+      .slice(0, 1);
+
+    largeCities.push(stringCity);
+  }
+  largeCitiesFormatted = largeCities.map((city) => {
+    for (const state of arrayState) {
+      if (state.ID === city[0].Estado) {
+        return `${city[0].Nome} - ${state.Sigla}`;
+      }
     }
+  });
 
-    console.log(cities);
+  console.log(largeCitiesFormatted);
 };
 
-// topFive("More");
+// largeCityForUf();
+// largeCityForUf("More");
+largeCityForUf();
+
 // topFive();
-moreStringCity();
+// topFive();
+// moreStringCity();
+
+// stringCity();
+// stringCity();
